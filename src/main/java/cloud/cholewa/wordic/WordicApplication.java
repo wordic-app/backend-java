41ec1c9ee0dea09d647288e50c5dae0196e32ea7
Krzysztof Cholewa - Ubuntu <krzysztof.cholewa@gmail.com>
2022-06-21 19:08:30 +0200

    [WOR] initial commit

---
+++ src/main/java/cloud/cholewa/wordic/WordicApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.wordic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/cholewa/wordic/WordicApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.wordic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
